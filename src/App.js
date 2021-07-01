e808aa3cc7a20fae14eae01e7dd6f5d4a47335bd
Silas Silva <silvasilas66@gmail.com>
2021-07-01 00:13:51 -0300

    Creating sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Sidebard from "./components/Sidebar/Sidebard";
 import Topbar from "./components/Topbar/Topbar";
+import './App.css';
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebard />
+        <div className="others">
+          adskajds
+        </div>
+      </div>
     </div>
   );
 }
